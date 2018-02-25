a2cb9f181792ab16ad8527112de2b95ef8deb142
Daniel Rocha <danielsantosr.rj@gmail.com>
2018-02-25 19:14:07 -0300

    start

---
+++ src/main/java/com/danielrocha/controladocs/ControlaDocsApplication.java
@@ -0,0 +1,12 @@
+package com.danielrocha.controladocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlaDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlaDocsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/danielrocha/controladocs/ControlaDocsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.danielrocha.controladocs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControlaDocsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
